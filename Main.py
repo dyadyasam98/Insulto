3fb755acd2f3b8bcb03131ef394174d07b61e071
dyadyasam98 <strogaya.tt@mail.ru>
2025-06-08 19:58:12 +0700

    Version 2.0
    /start

--- Main.py
+++ Main.py
@@ -16,21 +16,19 @@ not_m = generate_phrases(Not_mat)
 m = generate_phrases(Mat)
 no_matter = generate_phrases(You)
 
-def name_phrase(name, sex):
+def name_phrase(name, sex, mat_or_not):
   random_index = rnd.randint(0, len(m)-1)
   if sex == 'мужской':
-    mat_or_not = input('Приветствуете ли вы ненормативную лексику? (Введите да, нет или без разницы): ')
-    if mat_or_not == "да":
+    if mat_or_not == True:
       return f'{name}, ты {m[random_index]}'
-    elif mat_or_not == "нет":
+    elif mat_or_not == False:
       return f'{name}, ты {not_m[random_index % len(not_m) - 1]}'
     else:
       return f'{name}, {no_matter[random_index % len(no_matter) - 1]}'
   elif sex == 'женский':
-    mat_or_not = input('Приветствуете ли вы ненормативную лексику? (Введите да, нет или без разницы): ')
-    if mat_or_not == "да":
+    if mat_or_not == True:
       return f'{name}, ты {f_mat[random_index % len(f_mat) - 1]}'
-    elif mat_or_not == "нет":
+    elif mat_or_not == False:
       return f'{name}, ты {f_not_mat[random_index % len(f_not_mat) - 1]}'
     else:
       return f'{name}, {f[random_index % len(f) - 1]}'
@@ -45,27 +43,30 @@ def read_insalto(the_file):
         lines = file_to_read.readlines()
         for line in lines:
             print(line)
-    except:
+    except FileNotFoundError:
         print("Ошибка! Что-то пошло не так.")
 
 logs = open('logs.txt', 'a')
 
-print('Для прекращения работы программы в строке ввода имени введите "стоп"')
-number = 0
-while True:
-  number += 1
-  user_name = input('Введите имя: ')
-  if user_name == 'стоп':
-    print('Завершение работы программы по запросу пользователя.')
-    logs.close()
-    break
-  elif user_name == "инфо":
-    read_insalto('logs.txt')
-    break
-  user_sex = input('Введите пол: ')
-  insultion = name_phrase(user_name, user_sex)
-  print(insultion)
+def generate_insalto(number, user_name, user_sex, are_you_ready):
+    insultion = name_phrase(user_name, user_sex, are_you_ready)
+    logs.write(f'{number}. Name: {user_name}, Sex: {user_sex}, {insultion}')
+    logs.write('\n')
 
-  logs.write(f'{number}. Name: {user_name}, Sex: {user_sex}, {insultion}')
-  logs.write('\n')
-
+start_prog = input('Введите /generate_insulto, если схотите сгенерировать оскорбления. Или введите /print_all_insulto, если хотите увидеть уже имеющиеся оскорбления: ')
+if start_prog == '/generate_insulto':
+    r = input("Если вы поддерживаете использование мата, то введите: похуй, в противном случае введите: Экскьюзми")
+    number = 0
+    while True:
+        number += 1
+        user_name = input('Введите имя: ')
+        if user_name == "стоп":
+            print("Завершение работы программы по запросу пользователя.")
+            break
+        user_sex = input('Введите пол: ')
+        if r == "похуй":
+            insultion = generate_insalto(number, user_name, user_sex, True)
+        else:
+            insultion = generate_insalto(number, user_name, user_sex, False)
+elif start_prog == '/print_all_insulto':
+   read_insalto(logs)
